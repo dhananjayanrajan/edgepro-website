08c8369b30d4fc0a8db9c8252f73b5cc8fabbccc
Dhananjayan Rajan <dhananjayanrajan@gmail.com>
2026-05-16 01:51:49 +0530

    Background Checks

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,3 +1,4 @@
+import Beams from '../components/Beams'
 import { Outlet } from 'react-router-dom'
 import { useEffect } from 'react'
 import Lenis from 'lenis'
@@ -22,12 +23,26 @@ export default function PublicLayout() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-[#080808]">
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="min-h-screen bg-[#080808] relative">
+      <div style={{ width: '100%', height: '100%', position: 'fixed', top: 0, left: 0, opacity: 0.2, zIndex: 0, pointerEvents: 'none' }}>
+        <Beams
+          beamWidth={2}
+          beamHeight={30}
+          beamNumber={10}
+          lightColor="#ffffff"
+          speed={3}
+          noiseIntensity={1.5}
+          scale={0.1}
+          rotation={0}
+        />
+      </div>
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   )
 }
